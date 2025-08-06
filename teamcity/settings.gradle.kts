6066e7ddc56a500cd8012f1b48e8bb569511caee
K.H. Vishwanathan <khviyer@gmail.com>
2025-08-06 17:34:01 +0000

    Initial commit: Complete asset processing pipeline

---
+++ teamcity/settings.gradle.kts
@@ -0,0 +1,13 @@
+/*
+ * Author: Manus
+ * Date: 2025-08-05
+ * 
+ * This file is part of test assignment for "CI/CD Engineer" role in Scorewarrior
+ * 
+ * Test-assignment Version: 1.0.0
+ * Agent name and version: Gemini
+ */
+
+rootProject.name = "AssetPackagingPipeline"
+
+
